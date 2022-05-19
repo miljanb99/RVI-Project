16a225a0255767b7666a6ad531a21081512b179e
Minja Popović <prophethor@gmail.com>
2022-05-19 16:57:09 +0200

    Player movement and shooting

---
+++ Assets/PlayerShootingController.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShootingController : MonoBehaviour
+{
+    [SerializeField]
+    Transform weapon;
+    [SerializeField]
+    Rigidbody2D bulletPrefab;
+    [SerializeField]
+    float bulletSpeed = 10;
+
+
+    private void Update()
+    {
+        Aim();
+        Shoot();
+    }
+
+    private void Aim()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 aimDirection = (mousePos - weapon.position).normalized;
+        float aimAngle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg;
+        weapon.rotation = Quaternion.Euler(0, 0, aimAngle);
+    }
+
+    private void Shoot()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3 shootDirection = (mousePos - weapon.GetChild(0).position).normalized;
+            float shootAngle = Mathf.Atan2(shootDirection.y, shootDirection.x) * Mathf.Rad2Deg;
+            Rigidbody2D bullet = Instantiate(bulletPrefab, weapon.GetChild(0).position, Quaternion.Euler(0, 0, shootAngle));
+            bullet.velocity = shootDirection * bulletSpeed;
+        }
+    }
+
+}
