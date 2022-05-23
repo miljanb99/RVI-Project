13bf65dd8c5e8e185f05c5f7ab1518c994042f7d
miljanb99 <mi18240@alas.matf.bg.ac.rs>
2022-05-23 19:09:28 +0200

    levels design

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
@@ -13,6 +14,9 @@ public class MainMenu : MonoBehaviour
     }
 
     public void StartGame() {            
-        SceneManager.LoadScene("TestScene");
+        Dropdown[] dropdowns = FindObjectsOfType<Dropdown>();
+        int valueOfLevel = dropdowns[0].value;
+        string level = "Level" + (valueOfLevel+1).ToString();
+        SceneManager.LoadScene(level);
     }
 }
